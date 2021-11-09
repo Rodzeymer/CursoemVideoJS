5635c7060da8c95cff0bdd8a5d7293c9a82aba7c
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-09 13:49:51 -0300

    FOI!

--- exercicio12/funcoes.js
+++ exercicio12/funcoes.js
@@ -12,4 +12,10 @@ var mesHTML = window.document.querySelector('div#mes');
 mesHTML.innerHTML = `<p>O Mês atual é ${mes}</p>`;
 
 var diaHTML = window.document.querySelector('div#dia');
-diaHTML.innerHTML = `<p>O Ano atual é ${dia}</p>`;
+diaHTML.innerHTML = `<p>O Dia atual é ${dia}</p>`;
+
+var horaHTML = window.document.querySelector('div#hora')
+horaHTML.innerHTML = `<p>O Ano atual é ${hora}</p>`;
+
+var minutoHTML = window.document.querySelector('div#minuto')
+minutoHTML.innerHTML = `<p>O Ano atual é ${minuto}</p>`;
