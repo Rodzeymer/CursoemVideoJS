c292388d501994eb7c45801327fe543ae289aabc
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-09 13:48:10 -0300

    asoidjasiod

--- exercicio12/funcoes.js
+++ exercicio12/funcoes.js
@@ -5,21 +5,11 @@ var dia = agora.getDay();
 var hora = agora.getHours();
 var minuto = agora.getMinutes();
 
-var anoHTML = window.document.querySelector('div#ano').
-innerHTML = `<p>O Ano atual é ${ano}</p>`;
+var anoHTML = window.document.querySelector('div#ano');
+anoHTML.innerHTML = `<p>O Ano atual é ${ano}</p>`;
 
+var mesHTML = window.document.querySelector('div#mes');
+mesHTML.innerHTML = `<p>O Mês atual é ${mes}</p>`;
 
-var mes = window.document.querySelector('div#mes')
-mes.innerHTML = `<p>O Mês atual é ${mes}</p>`;
-
-var ano = window.document.querySelector('div#ano')
-ano.innerHTML = `<p>O Ano atual é ${ano}</p>`;
-
-var ano = window.document.querySelector('div#ano')
-ano.innerHTML = `<p>O Ano atual é ${ano}</p>`;
-
-var ano = window.document.querySelector('div#ano')
-ano.innerHTML = `<p>O Ano atual é ${ano}</p>`;
-
-var ano = window.document.querySelector('div#ano')
-ano.innerHTML = `<p>O Ano atual é ${ano}</p>`;
+var diaHTML = window.document.querySelector('div#dia');
+diaHTML.innerHTML = `<p>O Ano atual é ${dia}</p>`;
