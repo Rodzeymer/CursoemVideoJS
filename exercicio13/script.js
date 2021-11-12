9f080abea6bb93302e20e636ad8a720c26b0da80
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-12 09:58:45 -0300

    hora 13

--- exercicio13/script.js
+++ exercicio13/script.js
@@ -3,7 +3,10 @@ function carregar(){
     var msg = window.document.getElementById('msg');
     var img = window.document.getElementById('imagem');
     var data = new Date();
-    var hora = data.getHours();
+    //var hora = data.getHours();
+
+    var hota = 13;
+
     msg.innerHTML = `Agora são ${hora} horas!`
     if (hora >= 0 && hora <12){
         //BOM DIA
