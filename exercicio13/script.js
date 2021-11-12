dd94c7eeadbbe65bb85582de45181c3f72a39530
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-12 10:02:07 -0300

    22

--- exercicio13/script.js
+++ exercicio13/script.js
@@ -5,7 +5,7 @@ function carregar(){
     var data = new Date();
     //var hora = data.getHours();
 
-    var hora = 13;
+    var hora = 22;
 
     msg.innerHTML = `Agora são ${hora} horas!`
     if (hora >= 0 && hora <12){
