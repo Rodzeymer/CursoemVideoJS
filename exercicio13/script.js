984c4434e3417dc05db9371c135335b276845cd9
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-12 10:14:50 -0300

    scpritok

--- exercicio13/script.js
+++ exercicio13/script.js
@@ -10,13 +10,16 @@ function carregar(){
         //BOM DIA
         img.src = 'img13/morning.jpg';
         document.body.style.background ="#e3cd9f";
+        msg.innerHTML = `BOM DIA! Agora são ${hora} horas!`
     }else if (hora >=12 && hora <=18){
         //BOA TARDE
         img.src = 'img13/afternoon.jpg'
         document.body.style.background ="#f1da0a";
+        msg.innerHTML = `BOA TARDE! Agora são ${hora} horas!`
     }else{
         //BOA NOITE
         img.src = 'img13/evening.jpg'
         document.body.style.background ="#4925ca";
+        msg.innerHTML = `BOA NOITE! Agora são ${hora} horas!`
     }
 }
