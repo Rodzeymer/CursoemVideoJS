d7a28fb2c7bb579f1a51e7c125e132e1b9d906a4
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-12 15:48:58 -0300

    alterações mil

--- ex14/script.js
+++ ex14/script.js
@@ -11,13 +11,43 @@ function verificar(){
         var idade = ano - Number(formularioAno.value)
         //resultado.innerHTML = `Idade calculada: ${idade} anos`
         var genero =" ";
+        var img = document.createElement('img');
+        img.setAttribute('id', ', foto')
         if (formularioSexo[0].checked){
             genero = "Homem";
+            if(idade >=0 && idade< 10){
+                //Criança
+                img.setAttribute("src = ex14/img/menino-jovem.jpeg");
+            }else if(idade< 21){
+                //Jovem
+                img.setAttribute("src = ex14/img/homem-jovem.jpeg");
+            }else if(idade< 50){
+                //Adulto
+                img.setAttribute("src = ex14/img/homem-adulto.jpeg");
+            }else{
+                //Idoso
+                img.setAttribute("src = ex14/img/homem-idoso.jpeg");
+            }
         } else if (formularioSexo[1].checked){
             genero = "Mulher";
+            if(idade >=0 && idade< 10){
+                //Criança
+                img.setAttribute("src = ex14/img/menina-jovem.jpeg");
+            }else if(idade< 21){
+                //Jovem
+                img.setAttribute("src = ex14/img/mulher-jovem.jpeg");
+            }else if(idade< 50){
+                //Adulto
+                img.setAttribute("src = ex14/img/mulher-adulto.jpeg");
+            }else{
+                //Idoso
+                img.setAttribute("src = ex14/img/mulher-idoso.jpeg");
+            }
         } else if (formularioSexo[2].checked){
             genero = "Não-Binário";
         }
+        resultado.style.textAlign = 'center';
         resultado.innerHTML = `Detectamos ${genero} com ${idade} idade`
+
     }
 }
