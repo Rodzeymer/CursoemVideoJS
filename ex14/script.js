0df213afb1910b80d791538b25fdb7219c79d2cd
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-12 15:08:05 -0300

    Teste

--- ex14/script.js
+++ ex14/script.js
@@ -9,6 +9,14 @@ function verificar(){
     } else{
         var formularioSexo = document.getElementsByName('radsex')
         var idade = ano - Number(formularioAno.value)
-        resultado.innerHTML = `Idade calculada: ${idade} anos`
-    }
+        //resultado.innerHTML = `Idade calculada: ${idade} anos`
+        var genero ="";
+        if (formularioSexo[0].checked){
+            genero = "Homem";
+        } else if (formularioSexo[1].checked){
+            genero = "Mulher";
+        } else if (formularioSexo[2].checked){
+            genero = "Não-Binário";
+        }
+        resultado.innerHTML = ´Detectamos ${genero} com ${idade} idade´;
 }
