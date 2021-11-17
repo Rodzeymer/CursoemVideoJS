68ec752090539c6b8e086e6154e243eae9384813
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-17 11:22:05 -0300

    refeito

--- ex16/script.js
+++ ex16/script.js
@@ -2,9 +2,19 @@ function contar(){
     var inicio = document.getElementById("txtInicio");
     var fim = document.getElementById("txtFim");
     var passo = document.getElementById("txtPasso");
+    var resultado = document.getElementById("resultado")
 
-    for (let index = inicio.value; index < fim.value; index=index+passo.value) {
-        var contagem = document.createElement('p')
-        contagem.innerHTML = `${index}`
+    if(inicio.value.lenght == 0 || fim.value.lenght == 0|| passo.value.lenght == 0){
+        window.alert('[ERRO] Faltam dados!')
+    } else{
+        resultado.innerHTML = 'Contando: '
+        var numeroInicio = Number(inicio.value)
+        var numeroFim = Number(fim.value)
+        var numeroPasso = Number(passo.value)
+
+        for (let c = numeroInicio; index <= numeroFim; c+=numeroPasso) {
+            resultado.innerHTML +=`${c} `
+            
+        }
     }
 }
