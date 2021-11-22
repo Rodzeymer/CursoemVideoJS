79fa87df5aa40f9f42e393187e5c5880b98b0483
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-22 16:56:44 -0300

    valor

--- ex17/script.js
+++ ex17/script.js
@@ -1,11 +1,12 @@
 function tabuada(){
     var numero = document.getElementById("tabuada")
-    if (numero == null){
-        window.alert("Insira um número inteiro!")
+    var valor = Number(numero.value)
+    if (valor == ''){
+        window.alert("Insira um número inteiro positivo!")
     }else{
         for (let index = 0; index <= 10; index++) {
             var tabuada1 = document.createElement('p');
-            tabuada[index].innerHTML = numero * index;
+            tabuada[index].innerHTML = valor * index;
             var resultado = document.getElementById("resultado")
             resultado = document.innerHTML(tabuada[index])
             
