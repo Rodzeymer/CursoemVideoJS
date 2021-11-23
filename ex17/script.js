743a9812947575ad166d9f9f1d137e298a4112d9
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-23 08:15:02 -0300

    2

--- ex17/script.js
+++ ex17/script.js
@@ -5,7 +5,7 @@ function tabuada(){
         alert("Insira um número inteiro!")
     } else{
         for (let index = 0; index < 11; index++) {
-            var calculo,[index] = valor * array[index];
+            var calculo = valor * array[index];
             alert(calculo[index])
             
         }
