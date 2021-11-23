e5bb10921fb89a5217b9c823f6dbcc37dc859442
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-23 11:51:44 -0300

    7

--- ex17/script.js
+++ ex17/script.js
@@ -1,11 +1,12 @@
 function tabuada(){
     let numero = document.getElementById('numTabuada')
     let selecaoTabuada = document.getElementById('seltab')
+    let valor = Number(numero.value)
     
-    if(numero == ''){
+    if(valor == ''){
         alert('Porfavor digite um numero')
     } else {
         
-        let valor = Number(numero.value)
+        
     }
 }
