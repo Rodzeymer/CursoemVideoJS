92180459abc7c8f790719858acdedad13f35a950
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-23 08:01:42 -0300

    teste isolado

--- ex17/script.js
+++ ex17/script.js
@@ -1,15 +1,5 @@
 function tabuada(){
     var numero = document.getElementById("tabuada")
     var valor = Number(numero.value)
-    if (valor == ''){
-        window.alert("Insira um número inteiro positivo!")
-    }else{
-        for (let index = 0; index <= 10; index++) {
-            var tabuada1 = document.createElement('p');
-            tabuada[index].innerHTML = valor * index;
-            var resultado = document.getElementById("resultado")
-            resultado = document.innerHTML(tabuada[index])
-            
-        }
-    }
+    alert(`O número escolhido foi ${numero} com o valor de ${valor}`)
 }
