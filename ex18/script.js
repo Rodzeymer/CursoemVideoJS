948d7f0c2bb3e400dad6fa18cdfa212fe6d3ba17
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-24 10:02:42 -0300

    3

--- ex18/script.js
+++ ex18/script.js
@@ -1,6 +1,7 @@
 var listaNum = []
 function adicionar(){
     let numNovo = window.document.getElementById('numeroNovo')
+    listaNum.push(numNovo)
     if(listaNum.indexOf(numNovo) != -1){
         listaNum.push(numNovo)
     } else {
