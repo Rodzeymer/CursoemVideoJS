b03bc9bbce519329f4f3cc3388bea86e0bac1a3e
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-24 11:51:39 -0300

    3

--- ex18/script.js
+++ ex18/script.js
@@ -1,11 +1,10 @@
 var listaNum = []
 function adicionar(){
     let numNovo = window.document.getElementById('numeroNovo').value
-    if (listaNum == [''] || listaNum.indexOf(numNovo) == -1){
+    if (listaNum == [] || listaNum.indexOf(numNovo) == -1){
         listaNum.push(numNovo)
         window.alert(`Foi adicionado o ${numNovo} na lista`)
-    }else{
+    } else {
         window.alert('O número é inválido ou já está na lista!')
-        }
-    }
+        }    
 }
