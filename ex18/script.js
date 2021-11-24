c0dc8a3330713c1fe9877e6ca1dac54867579447
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-24 15:11:55 -0300

    segunda função

--- ex18/script.js
+++ ex18/script.js
@@ -1,7 +1,7 @@
 var listaNum = []
 var listaSoma = 0
 
-function adicionar(){
+function Adicionar(){
     let numNovo = window.document.getElementById('numeroNovo').value
     if (listaNum == [] || listaNum.indexOf(numNovo) == -1){
         listaSoma = listaSoma + numNovo
@@ -16,13 +16,16 @@ function adicionar(){
         }    
 }
 
-var listaOrdem = listaNum.sort()
-var quantidade = (listaNum.length)+1
-var menorNum = listaOrdem[0]
-var maiorNum = listaOrdem[quantidade]
-var mediaNum = listaSoma/quantidade
-console.log(`Quantidade de Itens adicionados ${quatidade}`)
-console.log(`Maior Número da sequencia ${maiorNum}`)
-console.log(`Menor Número da sequencia ${menorNum}`)
-console.log(`A soma dos itens adicionados é ${listaSoma}`)
-console.log(`A média dos itens adicionados é ${mediaNum}`)
+function Calcular(){
+    var listaOrdem = listaNum.sort()
+    var quantidade = (listaNum.length)+1
+    var menorNum = listaOrdem[0]
+    var maiorNum = listaOrdem[quantidade]
+    var mediaNum = listaSoma/quantidade
+    console.log(`Quantidade de Itens adicionados ${quantidade}`)
+    console.log(`Maior Número da sequencia ${maiorNum}`)
+    console.log(`Menor Número da sequencia ${menorNum}`)
+    console.log(`A soma dos itens adicionados é ${listaSoma}`)
+    console.log(`A média dos itens adicionados é ${mediaNum}`)
+
+}
