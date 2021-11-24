df6f8b59d92ca79ccc67edd9bfa15b7d7cb2a5a7
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-24 15:20:51 -0300

    soma

--- ex18/script.js
+++ ex18/script.js
@@ -4,7 +4,6 @@ var listaSoma = 0
 function Adicionar(){
     let numNovo = window.document.getElementById('numeroNovo').value
     if (listaNum == [] || listaNum.indexOf(numNovo) == -1){
-        listaSoma = listaSoma + numNovo
         listaNum.push(numNovo)
         window.alert(`Foi adicionado o ${numNovo} na lista`)
         let item = document.createElement('option')
@@ -18,7 +17,12 @@ function Adicionar(){
 
 function Calcular(){
     var listaOrdem = listaNum.sort()
-    var quantidade = (listaNum.length)+1
+    var listaSoma = 0
+    for (let index = 0; index < listaOrdem.length; index++) {
+        listaSoma += listaOrdem[index]
+        
+    }
+    var quantidade = listaOrdem.length
     var menorNum = listaOrdem[0]
     var maiorNum = listaOrdem[quantidade]
     var mediaNum = listaSoma/quantidade
