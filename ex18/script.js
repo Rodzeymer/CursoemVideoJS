9e5075b7e99802ad604afee9e66f5aef45da2a50
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-24 09:41:21 -0300

    alert numero velho

--- ex18/script.js
+++ ex18/script.js
@@ -1,6 +1,10 @@
 var listaNum = []
 function adicionar(){
     let numNovo = window.document.getElementById('numeroNovo').value
+    if(numNovo in listaNum){
+        window.alert("Número inválido ou já existente")
+    } else {
     listaNum.push(numNovo)
     window.alert(`${listaNum}`)
+    }
 }
