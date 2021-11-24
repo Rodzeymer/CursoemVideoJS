c03269917817485454bc1d14e704e4252c0c16e4
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-24 14:33:45 -0300

    segundas fuções

--- ex18/script.js
+++ ex18/script.js
@@ -1,7 +1,10 @@
 var listaNum = []
+var listaSoma = 0
+
 function adicionar(){
     let numNovo = window.document.getElementById('numeroNovo').value
     if (listaNum == [] || listaNum.indexOf(numNovo) == -1){
+        listaSoma = listaSoma + numNovo
         listaNum.push(numNovo)
         window.alert(`Foi adicionado o ${numNovo} na lista`)
         let item = document.createElement('option')
@@ -13,4 +16,13 @@ function adicionar(){
         }    
 }
 
-var quantidade = listaNum.lenght
+var listaOrdem = listaNum.sort()
+var quantidade = (listaNum.length)+1
+var menorNum = listaOrdem[0]
+var maiorNum = listaOrdem[quantidade]
+var mediaNum = listaSoma/quantidade
+console.log(`Quantidade de Itens adicionados ${quatidade}`)
+console.log(`Maior Número da sequencia ${maiorNum}`)
+console.log(`Menor Número da sequencia ${menorNum}`)
+console.log(`A soma dos itens adicionados é ${listaSoma}`)
+console.log(`A média dos itens adicionados é ${mediaNum}`)
