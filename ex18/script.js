52502178d6698b556317a664eb501df631bc59db
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-24 09:38:25 -0300

    crases??

--- ex18/script.js
+++ ex18/script.js
@@ -2,5 +2,5 @@ var listaNum = []
 function adicionar(){
     let numNovo = window.document.getElementById('numeroNovo').value
     listaNum.push(numNovo)
-    window.alert(listaNum.values)
+    window.alert(`${listaNum}`)
 }
