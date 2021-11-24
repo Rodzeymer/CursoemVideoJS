aaa1622452bd45ed5cb209c02878d310a1ccfed7
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-24 16:38:40 -0300

    idnet

--- ex18/script.js
+++ ex18/script.js
@@ -18,22 +18,23 @@ function Adicionar(){
 function Calcular(){
     if(listaNum == []){
         document.alert('Insira primeiro os valores a serem calculados')
+    } else {
+        var listaOrdem = listaNum.sort()
+        var listaSoma = 0
+        
+        var quantidade = (listaOrdem)
+        var menorNum = listaOrdem[0]
+        var maiorNum = listaOrdem[quantidade.value]
+        var mediaNum = listaSoma/quantidade
+        for (let index = 0; index < quantidade; index++) {
+            listaSoma = listaSoma + listaOrdem[index]        
+        }
+        console.log(`Quantidade de Itens adicionados ${quantidade}`)
+        console.log(`A lista é ${listaOrdem}`)
+        console.log(`Maior Número da sequencia ${maiorNum}`)
+        console.log(`Menor Número da sequencia ${menorNum}`)
+        console.log(`A soma dos itens adicionados é ${listaSoma}`)
+        console.log(`A média dos itens adicionados é ${mediaNum}`)
     }
-    var listaOrdem = listaNum.sort()
-    var listaSoma = 0
-    
-    var quantidade = (listaOrdem)
-    var menorNum = listaOrdem[0]
-    var maiorNum = listaOrdem[quantidade.value]
-    var mediaNum = listaSoma/quantidade
-    for (let index = 0; index < quantidade; index++) {
-        listaSoma = listaSoma + listaOrdem[index]        
-    }
-    console.log(`Quantidade de Itens adicionados ${quantidade}`)
-    console.log(`A lista é ${listaOrdem}`)
-    console.log(`Maior Número da sequencia ${maiorNum}`)
-    console.log(`Menor Número da sequencia ${menorNum}`)
-    console.log(`A soma dos itens adicionados é ${listaSoma}`)
-    console.log(`A média dos itens adicionados é ${mediaNum}`)
 
 }
