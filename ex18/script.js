9984460a66ee8a074c268399294d92cd7b99f388
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-25 09:28:26 -0300

    8

--- ex18/script.js
+++ ex18/script.js
@@ -1,44 +1,30 @@
-var listaNum = []
-var listaSoma = 0
+let novoNum = document.querySelector('input#numeroNovo')
+let listaNum = document.querySelector('select#numerosAdc')
+let resultado = document.querySelector('div#resultado')
+let valores = []
 
-function Adicionar(){
-    let numNovo = window.document.getElementById('numeroNovo').value
-    if (numNovo > 1 && numNovo <100){
-        if (numNovo != '' && listaNum.indexOf(numNovo) == -1){        
-            //window.alert(`Foi adicionado o ${numNovo} na lista`)
-            let item = document.createElement('option')
-                item.text = numNovo
-                item.value = `listaNum${numNovo}`
-                numerosAdc.appendChild(item)
-                listaNum.push(numNovo)
-        }else {
-            window.alert('O número é inválido ou já está na lista!')  
-        } 
-    } else {
-        window.alert('O número é inválido ou já está na lista!')  
+function isNumero(n){
+    if(Number(n) >=1 && Number(n) <= 100){
+        return true
+    } else{
+        return false
     }
+
 }
 
-function Calcular(){
-    if(listaNum == []){
-        document.alert('Insira primeiro os valores a serem calculados')
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1){
+        return true
     } else {
-        var listaOrdem = listaNum.sort()
-        var listaSoma = 0
-        
-        var quantidade = (listaOrdem)
-        var menorNum = listaOrdem[0]
-        var maiorNum = listaOrdem[quantidade.value]
-        var mediaNum = listaSoma/quantidade
-        for (let index = 0; index < quantidade; index++) {
-            listaSoma = listaSoma + listaOrdem[index]        
-        }
-        console.log(`Quantidade de Itens adicionados ${quantidade}`)
-        console.log(`A lista é ${listaOrdem}`)
-        console.log(`Maior Número da sequencia ${maiorNum}`)
-        console.log(`Menor Número da sequencia ${menorNum}`)
-        console.log(`A soma dos itens adicionados é ${listaSoma}`)
-        console.log(`A média dos itens adicionados é ${mediaNum}`)
+        return false
     }
 
+}
+
+function Adicionar(){
+    if(isNumero(novoNum.value) && !inLinsta(novoNum.value)){
+
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista')
+    }
 }
---
+++ ex18/script2.js
@@ -0,0 +1,44 @@
+var listaNum = []
+var listaSoma = 0
+
+function Adicionar(){
+    let numNovo = window.document.getElementById('numeroNovo').value
+    if (numNovo > 1 && numNovo <100){
+        if (numNovo != '' && listaNum.indexOf(numNovo) == -1){        
+            //window.alert(`Foi adicionado o ${numNovo} na lista`)
+            let item = document.createElement('option')
+                item.text = numNovo
+                item.value = `listaNum${numNovo}`
+                numerosAdc.appendChild(item)
+                listaNum.push(numNovo)
+        }else {
+            window.alert('O número é inválido ou já está na lista!')  
+        } 
+    } else {
+        window.alert('O número é inválido ou já está na lista!')  
+    }
+}
+
+function Calcular(){
+    if(listaNum == []){
+        document.alert('Insira primeiro os valores a serem calculados')
+    } else {
+        var listaOrdem = listaNum.sort()
+        var listaSoma = 0
+        
+        var quantidade = (listaOrdem)
+        var menorNum = listaOrdem[0]
+        var maiorNum = listaOrdem[quantidade.value]
+        var mediaNum = listaSoma/quantidade
+        for (let index = 0; index < quantidade; index++) {
+            listaSoma = listaSoma + listaOrdem[index]        
+        }
+        console.log(`Quantidade de Itens adicionados ${quantidade}`)
+        console.log(`A lista é ${listaOrdem}`)
+        console.log(`Maior Número da sequencia ${maiorNum}`)
+        console.log(`Menor Número da sequencia ${menorNum}`)
+        console.log(`A soma dos itens adicionados é ${listaSoma}`)
+        console.log(`A média dos itens adicionados é ${mediaNum}`)
+    }
+
+}
