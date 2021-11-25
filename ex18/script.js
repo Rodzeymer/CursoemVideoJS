53f24db5c76697ca87a217fe1a83ab80b351329a
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-25 08:38:05 -0300

    4

--- ex18/script.js
+++ ex18/script.js
@@ -2,14 +2,13 @@ var listaNum = []
 var listaSoma = 0
 
 function Adicionar(){
-    let numNovo = window.document.getElementById('numeroNovo')
-    let numNovoValor = numNovo.value
+    let numNovo = window.document.getElementById('numeroNovo').value
     if (listaNum == [] || listaNum.indexOf(numNovo) == -1 || numNovo < 100){
-        listaNum.push(numNovoValor)
+        listaNum.push(numNovo)
         //window.alert(`Foi adicionado o ${numNovo} na lista`)
         let item = document.createElement('option')
-            item.text = numNovoValor
-            item.value = `listaNum${numNovoValor}`
+            item.text = numNovo
+            item.value = `listaNum${numNovo}`
             numerosAdc.appendChild(item)
     } else {
         window.alert('O número é inválido ou já está na lista!')
