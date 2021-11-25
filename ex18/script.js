f596448d73c19c29575697b54f06170c268b5089
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-11-25 09:11:35 -0300

    6

--- ex18/script.js
+++ ex18/script.js
@@ -4,7 +4,7 @@ var listaSoma = 0
 function Adicionar(){
     let numNovo = window.document.getElementById('numeroNovo').value
     if (numNovo > 1 || numNovo <100){
-        if (numNovo == '' || listaNum.indexOf(numNovo) == -1){        
+        if (numNovo != '' || listaNum.indexOf(numNovo) == -1){        
             //window.alert(`Foi adicionado o ${numNovo} na lista`)
             let item = document.createElement('option')
                 item.text = numNovo
